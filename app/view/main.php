edf53a8ad4349afaf9d7c4a417b205720cf92977
frabetti7949 <frabetti.7949@isit100.fe.it>
2026-03-31 08:47:33 +0200

    logo caricato

---
+++ app/view/main.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookly</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <header>
+        <section>
+            <h1>Bookly</h1>
+        </section>
+    </header>
+    <main>
+        <div>
+            
+        </div>
+    </main>
+</body>
+</html>
