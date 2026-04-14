2dbec5b5fa4968514e2a7a095145bedca8f6c859
Giemme5 <facchichris@gmail.com>
2026-04-14 08:56:11 +0200

    feat:aggiunte funzioni nei model Main e personalArea e commenti correlati

--- app/models/MainModel.php
+++ app/models/MainModel.php
@@ -9,6 +9,14 @@ class mainModel{
         $this->pdo = DB::connect();
     }
 
-    //Qua è necessaria una funzione che estragga le ultime 3 inserzioni inserite dagli utenti
+    //Funzione che estragga le ultime 3 inserzioni inserite dagli utenti
+    public function select3Last(array $param=[]) : array{
+        $dql="SELECT * FROM insertions ORDER BY copy_id DESC LIMIT 3";
+
+        $stm=$this->pdo->prepare($dql);
+        $stm->execute($param);
+
+        return $stm->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 }
--- app/models/personalAreaModel.php
+++ app/models/personalAreaModel.php
@@ -11,8 +11,23 @@ class personalAreaModel{
     //qua serve una funzione che estragga tutte le inserzioni di un utente
 
     //funzione per aggiungere un'inserzione
+    public function addInsertion(array $param) :bool{
+        $dml="INSERT INTO insertions(`price`,`exchange_day`,`book_condition`,`sell_time`,`insertion_state`,`post_date`,`selling_user`,`buying_user`,`place_id`,`book_id`,`course_id`)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+        //$param contiene tutti i dati necessari
+        $stm=$this->pdo->prepare($dml); //prepara l'istruzione query ricevuta in $dml
+        $stm->execute($param); //esegue la query preparata usando come valori il contenuto dell'array $param
+        return $stm->rowCount()!==0; //controlla che l'inserimento sia andato a buon fine
+    }
 
     //funzione per modificare un'inserzione
 
     //funzione per eliminare un'inserzione
+    public function deleteInsertion(array $param): bool{
+        $dml="DELETE FROM insertions WHERE `insertion_id`=?";
+        //$param=[insertion_id]
+        $stm=$this->pdo->prepare($dml); //prepara l'istruzione query ricevuta in $dml
+        $stm->execute($param); //esegue la query preparata usando come valori il contenuto dell'array $param
+        return $stm->rowCount()!==0; //controlla che l'eliminazione sia andato a buon fine
+    }
 }
