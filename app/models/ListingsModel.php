2b28e503aa850fd70b4cecf1967ed53a553a8869
FedericoFerioli <federico.ferioli.2007@gmail.com>
2026-04-16 15:54:59 +0200

    query utili

--- app/models/ListingsModel.php
+++ app/models/ListingsModel.php
@@ -11,9 +11,16 @@ class listingsModel{
 
 
     //funzione che estragga tutti gli annunci
+    public function SelectAll(array $param=[]): array{
+        $dql ="SELECT * FROM insertions JOIN books USING book_id JOIN users USING selling_user JOIN courses USING course_id JOIN places Using place_id"; //riporta il contenuto della tabella insertions
+        $stm=$this->pdo->prepare($dql); //prepara la query ricevuta da $dql
+        $stm->execute($param); //esegue la query usando $param come contenitore per il risultato
+        return $stm->fetchAll(PDO::FETCH_ASSOC); //il risultato viene trasformato in array associativo
+    }
     
     //funzioni che filtrano bisogna pensare bene come fare, io per adesso non ho idee
-
+    public function filterSelect(array $param=[]): array{
+        $dql="SELECT * "
+    }
 
 }
-
