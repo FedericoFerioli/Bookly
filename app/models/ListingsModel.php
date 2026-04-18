5792ca2536a24f8792e36626fe1ae21213680dc0
Giemme5 <facchichris@gmail.com>
2026-04-18 09:45:36 +0200

    Funzioni di modifica di un inserzione, eliminazione di un inserzione, visualizzazione inserzioni di un utente e modifica SelectAll in ListingModel e di select3Last in MainModel

--- app/models/ListingsModel.php
+++ app/models/ListingsModel.php
@@ -13,9 +13,11 @@ class listingsModel{
     //funzione che estragga tutti gli annunci
     public function SelectAll(array $param=[]): array{
 
-        $dql ="SELECT * FROM insertions
-        join books USING(book_id)
-        join courses USING(course_id)"; //riporta il contenuto della tabella insertions
+        $dql ="SELECT insertions.*, books.title, books.authors, books.publisher, users.name, users.surname, subjects.name as subject_name
+            FROM insertions 
+            JOIN books USING(book_id) 
+            JOIN users ON insertions.selling_user = users.user_id
+            JOIN subjects USING(subject_id)"; //riporta il contenuto della tabella insertions prendendo anche titolo del libro, autore, materia e utenti dalle altre tabelle
 
         $stm=$this->pdo->prepare($dql); //prepara la query ricevuta da $dql
         $stm->execute($param); //esegue la query usando $param come contenitore per il risultato
--- app/models/MainModel.php
+++ app/models/MainModel.php
@@ -11,7 +11,13 @@ class mainModel{
 
     //Funzione che estragga le ultime 3 inserzioni inserite dagli utenti
     public function select3Last(array $param=[]) : array{
-        $dql="SELECT * FROM insertions ORDER BY insertion_id DESC LIMIT 3"; //query che prende le ultime 3 inserzioni aggiunte
+        $dql="SELECT insertions.*, books.title, books.authors, books.publisher, users.name AS `name`, users.surname AS `surname`, subjects.name AS subject_name
+            FROM insertions 
+            LEFT JOIN books USING(book_id) 
+            LEFT JOIN users ON insertions.selling_user = users.user_id
+            LEFT JOIN subjects USING(subject_id)
+            ORDER BY insertion_id DESC
+            LIMIT 3"; //query che prende le ultime 3 inserzioni aggiunte
 
         $stm=$this->pdo->prepare($dql); //prepara la query ricevuta da $dql
         $stm->execute($param); //esegue la query e usa $param come contenitore per il risultato
